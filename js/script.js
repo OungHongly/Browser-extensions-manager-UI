4995431a48bb15ac8f4d4052044fa54c57070b5a
Hongly <honglyoung.123@gmail.com>
2025-09-06 21:21:58 +0700

    complete dark mode

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 "use strict";
+
+// selectes elements
+const btnTheme = document.getElementById("btn--theme");
+const body = document.querySelector("body");
+const logo = document.getElementById("logo");
+const themeIcon = document.getElementById("theme--icon");
+
+btnTheme.addEventListener("click", () => {
+  body.classList.toggle("dark-mode");
+
+  if (body.classList.contains("dark-mode")) {
+    logo.src = "./assets/images/logo.png";
+    themeIcon.src = "./assets/images/icon-sun.svg";
+  } else {
+    logo.src = "./assets/images/logo.svg";
+    themeIcon.src = "./assets/images/icon-moon.svg";
+  }
+});
